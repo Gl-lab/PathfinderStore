07530d557ae3b7d735acba4fcdfe555993284478
Gl-lab <Gl-lab@live.ru>
2020-05-19 22:11:46 +0300

    Разобрался как пользоватся таблицами во vuetify, в планах переделать на адекватное получение данных для таблицы, а не то что я натворил сейчас

--- Pathfinder.Web/ClientApp/src/models/Category.ts
+++ Pathfinder.Web/ClientApp/src/models/Category.ts
@@ -1,9 +1,9 @@
-import { ICategory } from './Interfaces/ICategory'
+import { ICategory } from './Interfaces/ICategory';
 export class Category implements ICategory {
-    id: number;
-    name: string;
-    description: string;
-    constructor (id: number, name: string, description: string){
+    public id: number;
+    public name: string;
+    public description: string;
+    constructor(id: number, name: string, description: string) {
         this.id = id;
         this.name = name;
         this.description = description;
