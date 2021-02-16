3a94d65dc4485e48eeda01080bdc99c5daf4ca4e
Orlov Alexander <Gl-lab@live.ru>
2021-02-16 22:46:25 +0300

    Добавлены характеристики в создание персонажа

---
+++ Pathfinder.Web/ClientApp/src/models/Characteristics.js
@@ -0,0 +1,29 @@
+class Characteristics {
+  constructor() {
+    this.strength = {
+      value: 0,
+      modifier: null
+    };
+    this.dexterity = {
+      value: 0,
+      modifier: null
+    };
+    this.constitution = {
+      value: 0,
+      modifier: null
+    };
+    this.intelligence = {
+      value: 0,
+      modifier: null
+    };
+    this.wisdom = {
+      value: 0,
+      modifier: null
+    };
+    this.charisma = {
+      value: 0,
+      modifier: null
+    };
+  }
+}
+export default Characteristics;
